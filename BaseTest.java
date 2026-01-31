a343e2e73e44b019df7329cb5a42e9d08afe2b9c
Mohit <119913939+mohitpoonia046@users.noreply.github.com>
2026-01-31 10:29:59 +0530

    Add project documentation to BaseTest.java
    Added documentation for automated testing setup using Selenium.

---
+++ BaseTest.java
@@ -0,0 +1,22 @@
+# Automated Login and Form Validation using Selenium
+
+## Tech Stack
+- Selenium WebDriver
+- Java
+- TestNG
+- Maven
+- XPath & CSS Selectors
+
+## Features
+- Automated Login Testing
+- Form Validation Testing
+- Logout Testing
+- Positive & Negative Scenarios
+
+## How to Run
+1. Clone the repository
+2. Import as Maven project
+3. Run `testng.xml`
+
+## Author
+Mohit Poonia
